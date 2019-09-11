2a4616871fe2ef2ca840f73777c86ab3e16e2e21
Collin Poczatek <collin.poczatek@gmail.com>
2019-09-11 14:03:54 -0700

    close ports

--- simple/network.tf
+++ simple/network.tf
@@ -1,3 +1,9 @@
+locals {
+  tcp_protocol  = "6"
+  all_protocols = "all"
+  anywhere      = "0.0.0.0/0"
+}
+
 data "oci_identity_availability_domains" "availability_domains" {
   compartment_id = "${var.compartment_ocid}"
 }
@@ -31,15 +37,48 @@ resource "oci_core_security_list" "security_list" {
   compartment_id = "${var.compartment_ocid}"
   vcn_id         = "${oci_core_virtual_network.virtual_network.id}"
 
-  egress_security_rules = [{
-    protocol    = "All"
-    destination = "0.0.0.0/0"
-  }]
+  egress_security_rules = [
+    {
+      protocol    = "${local.all_protocols}"
+      destination = "${local.anywhere}"
+    },
+  ]
 
-  ingress_security_rules = [{
-    protocol = "All"
-    source   = "0.0.0.0/0"
-  }]
+  ingress_security_rules {
+    protocol = "${local.tcp_protocol}"
+    source   = "${local.anywhere}"
+
+    tcp_options {
+      max = "22"
+      min = "22"
+    }
+  }
+
+  ingress_security_rules {
+    protocol = "${local.all_protocols}"
+    source   = "10.0.0.0/16"
+  }
+
+  ingress_security_rules {
+    protocol = "${local.tcp_protocol}"
+    source   = "${local.anywhere}"
+
+    tcp_options {
+      max = "8080"
+      min = "8080"
+    }
+  }
+
+  ingress_security_rules {
+    protocol = "${local.tcp_protocol}"
+    source   = "${local.anywhere}"
+
+    tcp_options {
+      max = "8088"
+      min = "8088"
+    }
+  }
+
 }
 
 resource "oci_core_subnet" "subnet" {
