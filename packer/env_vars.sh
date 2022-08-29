5659bfa8dc83a39aff6dbf7a560931e56d93b8b8
Collin Poczatek <collin.poczatek@gmail.com>
2022-08-29 10:47:45 -0700

    basic install

--- packer/env_vars.sh
+++ packer/env_vars.sh
@@ -2,3 +2,13 @@ export PACKER_availibilty_domain=""
 export PACKER_compartment_ocid=""
 export PACKER_subnet_ocid=""
 export PACKER_my_secret=""
+
+
+# Oracle-Linux-8.6-Gen2-GPU-2022.06.30-0 = ocid1.image.oc1.iad.aaaaaaaa5r5rrmygz247s5z7lco3p5oghiuazqfifalxodpvrx4b3uppiuwa
+# Oracle-Linux-8.6-2022.08.08-0 = ocid1.image.oc1.iad.aaaaaaaanpammf3j2xcbcoykbwlk3m35ko6svsdj2sut2fjtmnmujcnh3pvq
+export PACKER_base_image=""
+
+# "VM.Standard2.1"
+# "VM.GPU3.1"
+# "VM.Standard.E3.Flex"
+export PACKER_shape="VM.Standard2.1"
--- packer/installer.sh
+++ packer/installer.sh
@@ -1,3 +1,34 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-echo "Hello World!" > HelloWorld.txt
+echo "Gathering metadata..."
+
+public_ip=$(oci-public-ip -j | jq -r '.publicIp')
+private_ip=$(hostname -i)
+
+json=$(curl -sSL http://169.254.169.254/opc/v1/instance/)
+shape=$(echo $json | jq -r .shape)
+
+echo "Pub/priv IP: $public_ip $private_ip $shape"
+echo "Full metadata: $json"
+echo "Shape: $shape"
+
+echo "Kinetica install/setup"
+
+systemctl stop firewalld
+systemctl disable firewalld
+
+echo "Running yum install"
+wget -O /etc/yum.repos.d/kinetica-7.1.repo http://repo.kinetica.com/yum/7.1/CentOS/7/x86_64/kinetica-7.1.repo
+
+if [[ $shape == *"GPU"* ]]; then
+  echo "Running on GPU shape, installing cuda build..."
+  # cuda91 is correct for all GPU shapes as all GPUs are either V100/P100, T4 on rover
+  yum install -y gpudb-cuda91-license.x86_64
+  NUM_GPU=$(nvidia-smi -L | wc -l)
+  echo "Found the following number of GPUS: $NUM_GPU"
+else
+  echo "Running on non-GPU shape, installing intel build..."
+  yum install -y gpudb-intel-license.x86_64
+  NUM_NUMA=$(lscpu | awk -F":" '/^NUMA node\(s\)/ { print $2 }' | tr -d ' ')
+  echo "Found the following number of NUMA nodes: $NUM_NUMA"
+fi
